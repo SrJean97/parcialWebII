48e6d5e56a887a48db8c4948b3d604865ce47fb8
jean <jheancarlos1997123@gmail.com>
2020-11-12 21:31:41 -0500

    Agregados modelos backend

--- Entity/Persona.cs
+++ Entity/Persona.cs
@@ -5,6 +5,11 @@ namespace Entity
     public class Persona
     {
         [Key]
-        public string id { get; set; }
+        public string Id { get; set; }
+        public string Tipo { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Nacimiento { get; set; }
+        public string Institucion { get; set; }
+        public string Acudiente { get; set; }
     }
 }
--- Entity/Vacuna.cs
+++ Entity/Vacuna.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Entity
 {
     public class Vacuna
     {
-        
+        [Key]
+        public int Id { get; set; }
+        public Persona Persona { get; set; }
+        public string Tipo { get; set; }
+        public int Edad { get; set; }
+        public DateTime Aplicado { get; set; }
     }
 }
